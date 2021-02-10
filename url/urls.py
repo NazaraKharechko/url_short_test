e3e6b6a56c4bc8ed35230dd274d4bc36e611ea60
Admin <nazarkharechko90@gmail.com>
2021-02-10 16:08:55 +0200

    redirekt -)

--- url/urls.py
+++ url/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # path('', views.mam),
     path('', LinkCreate.as_view(), name='home'),
     path('link/<int:pk>', LinkShow.as_view(), name='link_show'),
-    path(r'r/<short_url>', RedirectToLongURL.as_view(), name='redirect_short_url'),
+    path('r/<short_url>', RedirectToLongURL.as_view(), name='redirect_short_url'),
 ]
